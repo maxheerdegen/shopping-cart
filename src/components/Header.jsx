18b578eb754ec4bbec67e6275ff7dd483bb64b56
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2025-04-11 12:42:25 +0200

    add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header () {
     return (
-        <>
+        <header>
             <div>My awesome shop</div>
             <div>
-                <ul>
+                <ul className={styles.navBar}>
                     <li><Link to="homepage">Homepage</Link></li>
                     <li><Link to="Products">Products</Link></li>
                     <li><Link to="Cart">Cart</Link></li>
                 </ul>
             </div>
-        </>
+        </header>
     )
 }
 
